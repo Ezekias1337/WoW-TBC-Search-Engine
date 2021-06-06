8038ba5f6b0e4aeef91710c130683157291aba5f
Frank Edwards <frankedwards50@gmail.com>
2021-06-06 14:49:35 -0700

    Arena Point Calculator Base functions

--- js/arenaPointCalculator.js
+++ js/arenaPointCalculator.js
@@ -4,11 +4,11 @@ function calculatePoints2s () {
     points = document.getElementById("2s").value;
 
     if (points >= 1500) {
-        pointOutput2s = ((1511.26/(1+1639.28*Math.pow(2.71828, -0.00412*points))) * 0.76)
+        pointOutput2s = Math.floor(((1511.26/(1+1639.28*Math.pow(2.71828, -0.00412*points))) * 0.76))
         console.log(pointOutput2s);
         document.getElementById("2s-output").value = pointOutput2s;
     } else if (points >= 200) {
-        pointOutput2s = (162 + ((points-200) * 0.0761538461538462))
+        pointOutput2s = Math.floor((162 + ((points-200) * 0.0761538461538462)))
         console.log(pointOutput2s);
         document.getElementById("2s-output").value = pointOutput2s;
     } else {
@@ -22,11 +22,11 @@ function calculatePoints3s () {
     points = document.getElementById("3s").value;
 
     if (points >= 1500) {
-        pointOutput3s = ((1511.26/(1+1639.28*Math.pow(2.71828, -0.00412*points))) * 0.88)
+        pointOutput3s = Math.floor(((1511.26/(1+1639.28*Math.pow(2.71828, -0.00412*points))) * 0.88))
         console.log(pointOutput3s);
         document.getElementById("3s-output").value = pointOutput3s;
     } else if (points >= 200) {
-        pointOutput3s = (188 + ((points-200) * 0.0876923076923077))
+        pointOutput3s = Math.floor((188 + ((points-200) * 0.0876923076923077)))
         console.log(pointOutput3s);
         document.getElementById("3s-output").value = pointOutput3s;
     } else {
@@ -40,11 +40,11 @@ function calculatePoints5s () {
     points = document.getElementById("5s").value;
 
     if (points >= 1500) {
-        pointOutput5s = (1511.26/(1+1639.28*Math.pow(2.71828, -0.00412*points)))
+        pointOutput5s = Math.floor((1511.26/(1+1639.28*Math.pow(2.71828, -0.00412*points))))
         console.log(pointOutput5s);
         document.getElementById("5s-output").value = pointOutput5s;
     } else if (points >= 200) {
-        pointOutput5s = (214 + ((points-200) * 0.1))
+        pointOutput5s = Math.floor((214 + ((points-200) * 0.1)))
         console.log(pointOutput5s);
         document.getElementById("5s-output").value = pointOutput5s;
     } else {
@@ -80,6 +80,40 @@ function resetArenaCalculator() {
     document.getElementById("5s").value = "";
     document.getElementById("5s-output").value = "";
 }
+
+document.getElementById("2s").addEventListener("keyup", function(event) {
+    var x = event.key;
+    // Number 13 is the "Enter" key on the keyboard
+    if (x === "Enter") {
+
+        
+        // Trigger the button element with a click
+        
+        document.getElementById("submitArenaPoints").click();
+}});
+
+document.getElementById("3s").addEventListener("keyup", function(event) {
+    var x = event.key;
+    // Number 13 is the "Enter" key on the keyboard
+    if (x === "Enter") {
+
+        
+        // Trigger the button element with a click
+        
+        document.getElementById("submitArenaPoints").click();
+}});
+
+document.getElementById("5s").addEventListener("keyup", function(event) {
+    var x = event.key;
+    // Number 13 is the "Enter" key on the keyboard
+    if (x === "Enter") {
+
+        
+        // Trigger the button element with a click
+        
+        document.getElementById("submitArenaPoints").click();
+}});
+
 
 document.getElementById("submitArenaPoints").addEventListener("click", submitAll);
 
